fd73688cd8cb3b4f3f2380eba09130b39ce1d462
Qiuxia Fan <fine0830@outlook.com>
2021-01-05 15:14:41 +0800

    fix: config

--- vue.config.js
+++ vue.config.js
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const serviceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   devServer: {
@@ -26,13 +24,6 @@ module.exports = {
         changeOrigin: true,
       },
     },
-  },
-  configureWebpack: {
-    plugins: [
-      new serviceWorkerWebpackPlugin({
-        entry: path.join(__dirname, './node_modules/skywalking-client-js/lib/sw.js'),
-      }),
-    ],
   },
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
